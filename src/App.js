314dd0f34bab8b0c215c4afe3749ad972fcc87ee
GeorgeCobb <70028860+GeorgeCobb@users.noreply.github.com>
2021-03-02 10:25:22 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React, {useState, useRef} from 'react'
 import './App.css';
+import TodoList from './TodoList'
+//port uuidv8 from 'uuid/v4'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const todonameref = useRef()
+/*
+  function number(){
+    if(prevTodos.id === NaN){
+      return 1
+    }else{
+      return prevTodos.id + 1
+    }
+  }
+*/
+  function handleAddTodo(e){
+    const name = todonameref.current.value
+    if(name === '') return
+    setTodos(prevTodos =>{
+      return[...prevTodos, {id: 1, name: name, complete: false}]
+    })
+    todonameref.current.value = null
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <TodoList todos = {todos}/>
+    <input ref = {todonameref}type = "text" />
+    <button onClick={handleAddTodo}>Add Todo</button>
+    <button> Clear Todo</button>
+    <div> 0 Left to do</div>
+    </>
   );
 }
 
